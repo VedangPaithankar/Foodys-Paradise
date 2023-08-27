f067c0bb230c72db742bba40310dd75c7c4305fd
vedangp12 <vedang.paithankar2021@vitstudent.ac.in>
2023-08-27 23:33:30 +0530

    Responsive Front End 2

--- FoodysParadise - FrontEnd/src/components/Navbar.jsx
+++ FoodysParadise - FrontEnd/src/components/Navbar.jsx
@@ -1,49 +1,39 @@
-import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import './Navbar.css';
 
 export default function Navbar(props) {
-  const [menuOpen, setMenuOpen] = useState(false);
-
-  const toggleMenu = () => {
-    setMenuOpen(!menuOpen);
-  };
-
-  return (
-    <div className={`flex align-items-center custom-font navbar ${menuOpen ? 'menu-open' : ''}`}>
-      <div className="nav-brand ml-5 mt-3 w-20 h-20 mb-3">
-        <img src="/Assets/Logo.svg" alt="" />
-      </div>
-      <Link className={`ml-2 text-black ${menuOpen ? 'hidden' : ''}`} to="/">
-        {props.title}
-      </Link>
-      <button className="menu-icon ml-auto mr-4" onClick={toggleMenu}>
-        <span className="dot"></span>
-        <span className="dot"></span>
-        <span className="dot"></span>
-      </button>
-      <ul className={`nav nav-underline ${menuOpen ? 'mobile-menu' : 'hidden'}`}>
-        <li className="nav-item">
-          <Link className="nav-link text-black" to="/">
-            Home
-          </Link>
-        </li>
-        <li className="nav-item">
-          <Link className="nav-link text-black" to="/search">
-            Search
-          </Link>
-        </li>
-        <li className="nav-item">
-          <Link className="nav-link text-black" to="/myfridge">
-            My Fridge
-          </Link>
-        </li>
-      </ul>
-    </div>
-  );
+    return (
+        <div className="flex align-items-center custom-font navbar">
+            <div className="nav-brand ml-10 mt-3 w-20 h-20 mb-3 logoadjust">
+                <img src="/Assets/Logo.svg" alt="" />
+            </div>
+            <div className="ml-2 mr-auto"> {/* Use mr-auto to push text to the right */}
+                <Link className="text-black hidtitle" to="/">
+                    {props.title}
+                </Link>
+            </div>
+            <ul className="navcen nav nav-underline">
+                <li className="nav-item">
+                    <Link className="nav-link text-black" to="/">
+                        Home
+                    </Link>
+                </li>
+                <li className="nav-item">
+                    <Link className="nav-link text-black" to="/search">
+                        Search
+                    </Link>
+                </li>
+                <li className="nav-item mr-10">
+                    <Link className="nav-link text-black" to="/myfridge">
+                        My Fridge
+                    </Link>
+                </li>
+            </ul>
+        </div>
+    );
 }
 
 Navbar.propTypes = {
-  title: PropTypes.string,
+    title: PropTypes.string,
 };
