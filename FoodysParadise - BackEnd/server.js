406876d7ffbb133d4556d5d5e8c168f7309e5e89
vedangp12 <vedang.paithankar2021@vitstudent.ac.in>
2024-08-10 19:06:45 +0530

    hello world msg

--- FoodysParadise - BackEnd/server.js
+++ FoodysParadise - BackEnd/server.js
@@ -1,32 +1,38 @@
-  const express = require("express");
-  const mongoose = require("mongoose");
-  require("dotenv").config();
-  const cors = require("cors");
-  const path = require("path"); // Import path module
+const express = require("express");
+const mongoose = require("mongoose");
+require("dotenv").config();
+const cors = require("cors");
+const path = require("path"); // Import path module
 
-  const routes = require("./routes/Route");
-  const app = express();
-  const PORT = process.env.PORT || 5000;
+const routes = require("./routes/Route");
+const app = express();
+const PORT = process.env.PORT || 5000;
 
-  app.use(express.json());
-  app.use(cors());
+app.use(express.json());
+app.use(cors());
 
-  mongoose
-    .connect(process.env.MONGO_URI)
-    .then(() => console.log("MongoDB Connected..."))
-    .catch((err) => console.log(err));
+// Connect to MongoDB
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB Connected..."))
+  .catch((err) => console.log(err));
 
-  app.use("/api", routes);
+// Define a test route to check server status
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
 
-  // Serve static assets if in production
-  if (process.env.NODE_ENV === "production") {
-    // Set static folder
-    app.use(express.static('../FoodysParadise - FrontEnd/build'));
+app.use("/api", routes);
 
-    // For any request that doesn't match a specific route, serve index.html
-    app.get("*", (req, res) => {
-      res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-    }); 
-  }
+// Serve static assets if in production
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static(path.join(__dirname, "../FoodysParadise - FrontEnd/build")));
 
-  app.listen(PORT, () => console.log(`Listening at ${PORT}`));
+  // For any request that doesn't match a specific route, serve index.html
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "../FoodysParadise - FrontEnd/build", "index.html"));
+  }); 
+}
+
+app.listen(PORT, () => console.log(`Listening at ${PORT}`));
